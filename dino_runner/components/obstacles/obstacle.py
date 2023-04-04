7a999afa55d6460357ef801440e1a2e28bb4f0ba
NicolasJALA <129456797+NicolasJALA@users.noreply.github.com>
2023-04-03 21:53:40 -0400

    Clase 3: Obstaculos2

--- dino_runner/components/obstacles/obstacle.py
+++ dino_runner/components/obstacles/obstacle.py
@@ -4,14 +4,13 @@ class Obstacle(Sprite):
     def __init__(self, image, type):
         self.image = image
         self.type = type
-        self.rect = self.image.get_rect()
+        self.rect = self.image[self.type].get_rect()
         self.rect.x = SCREEN_WIDTH
 
     def update(self, game_speed, obstacles):
         self.rect.x -= game_speed
-        print(self.rect.x)
         if self.rect.x < 0:
-            obstacles.pop()
+            self.obstacles.pop()
 
     def draw(self, SCREEN):
         # pygame.draw.rect(SCREEN, (0, 0, 0), self.rect, 2)
